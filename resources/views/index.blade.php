f34a9faf63724082bc8fe2c06e163cdc72e8f1e7
trịnh phương <trinhphuong@MacBook-Pro-cua-trinh.local>
2023-03-21 22:55:02 +0700

    frs commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <h2> blogs 1 </h2>
+                <p class="lead"> 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus cupiditate provident sint nam ut
+                    corrupti.
+                </p>
+                <a href="#"> Read more</a>
+            </div>
+
+        </div>
+    </div>
+@endsection
